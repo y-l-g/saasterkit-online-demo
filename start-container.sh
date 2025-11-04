41cd33e178729a76993165f2698dc5341a0d4b0d
YLG-dev <youenn.legouedec@gmail.com>
2025-11-05 00:24:24 +0100

    fix:build js in start-container instead dockerfile

--- start-container.sh
+++ start-container.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 cp /run/secrets/${APP_NAME}_ENV .env
+npm run build:ssr
+sed -i 's/payload: { serverRendered: false }/payload: { serverRendered: typeof window === "undefined"}/g' node_modules/@nuxt/ui/dist/runtime/inertia/stubs.js
 php /app/artisan optimize
 php /app/artisan storage:link
 exec "$@"
