b16c140a9ea1c7607092d5836386388d3386aed4
YLG-dev <youenn.legouedec@gmail.com>
2025-11-05 16:20:44 +0100

    doc: add content

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -51,7 +51,6 @@ class AuthServiceProvider extends ServiceProvider
             });
         }
     }
-
     private function registerFeatureGates(): void
     {
         foreach (FeatureEnum::cases() as $feature) {
